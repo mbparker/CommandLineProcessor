0dddd9e4babd5fa681b47889bb8cf4bee7ea516c
Matt Parker <mb.parker@gmail.com>
2018-05-25 09:09:59 -0400

    Add tests for method call validator

--- CommandLineProcessor/CommandLineProcessorLib/MethodCallValidatorProvider.cs
+++ CommandLineProcessor/CommandLineProcessorLib/MethodCallValidatorProvider.cs
@@ -51,6 +51,10 @@
                     }
                 }
             }
+            else
+            {
+                signatureMatches = false;
+            }
 
             if (signatureMatches)
             {
